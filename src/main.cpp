79dd57871b61f59b5a34a98a983c93181cbaae60
JamesBremner <james@ravenspoint.com>
2024-04-20 13:41:23 -0400

    Check for feasibility

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,183 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
 
-class cGUI : public cStarterGUI
+class cPlayer
 {
 public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
-    {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+    std::string myName;
+    std::vector<int> myTimes;
+    std::vector<std::string> myOpps;
 
-        show();
-        run();
+    cPlayer(const std::string name)
+        : myName(name)
+    {
     }
 
-private:
-    wex::label &lb;
+    void addTime(int t)
+    {
+        myTimes.push_back(t);
+    }
+    void addOpp(const std::string &opp)
+    {
+        myOpps.push_back(opp);
+    }
+
+    bool operator==(std::string &name)
+    {
+        return myName == name;
+    }
 };
 
+class cCourt
+{
+public:
+    int myTime;
+    cCourt(int t)
+        : myTime(t)
+    {
+    }
+};
+
+class cTask
+{
+public:
+    cPlayer &p1;
+    cPlayer &p2;
+    int myt;
+    cTask(
+        cPlayer &a,
+        cPlayer &b,
+        int t)
+        : p1(a),
+          p2(b),
+          myt(t)
+    {
+    }
+    void display();
+    static void displayAll();
+    static bool find( 
+        const std::string& p1,
+         const std::string& p2);
+};
+
+std::vector<cPlayer> thePlayers;
+std::vector<cCourt> theCourts;
+std::vector<cTask> theTasks;
+
+void cTask::displayAll()
+{
+    for (auto &t : theTasks)
+    {
+        t.display();
+    }
+}
+
+ bool cTask::find( const std::string& p1n, const std::string& p2n )
+ {
+    for (auto &t : theTasks)
+        if( t.p1.myName == p1n && t.p2.myName == p2n )
+            return true;
+    return false;
+ }
+void cTask::display()
+{
+    std::cout << p1.myName << " v " << p2.myName << " at " << myt << "\n";
+}
+
+void generate1()
+{
+    // generate players
+    thePlayers.emplace_back("pA");
+    thePlayers.emplace_back("pB");
+
+    // add possible opponents
+    thePlayers[0].addOpp("pB");
+    thePlayers[1].addOpp("pA");
+
+    // add available times
+    thePlayers[0].addTime(1);
+    thePlayers[0].addTime(3);
+    thePlayers[0].addTime(2);
+    thePlayers[1].addTime(2);
+    thePlayers[1].addTime(3);
+
+    // generate court times
+    theCourts.emplace_back(1);
+    theCourts.emplace_back(2);
+    theCourts.emplace_back(3);
+}
+int findPlayer(const std::string &name)
+{
+    for (int ip = 0; ip < thePlayers.size(); ip++)
+        if (thePlayers[ip].myName == name)
+            return ip;
+    return -1;
+}
+
+/**
+ * @brief Check for feasibility
+ *
+ * i.e. every player has at least one opponent with a matching available time and possible court time
+ */
+void check()
+{
+    // loop over players
+    int pi = -1;
+    for (auto &p : thePlayers)
+    {
+        pi++;
+
+        // assume that player has no possible opponent with matching time
+        bool ok = false;
+
+        // loop over player available times
+        for (int t : p.myTimes)
+        {
+            // loop over player possible opponents
+            for (auto &os : p.myOpps)
+            {
+                // loop over opponents available times
+                int oi = findPlayer(os);
+                for (int ot : thePlayers[oi].myTimes)
+                {
+                    if (t == ot)
+                    {
+                        // loop over court times
+                        for (auto &c : theCourts)
+                        {
+                            if (t == c.myTime)
+                            {
+                                // found a match with opponent and court
+
+                                ok = true;
+
+                                // create a task for each feasible pair
+                                // only record task when player 1 index less than player 2
+                                if (pi < oi) {
+
+
+
+                                    theTasks.emplace_back(p, thePlayers[oi], t);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (!ok)
+        {
+            std::cout << "Cannot find opponent for " << p.myName << "\n";
+            exit(1);
+        }
+    }
+    std::cout << "found feasible opponents for all players\n";
+    cTask::displayAll();
+}
 main()
 {
-    cGUI theGUI;
+    generate1();
+    check();
     return 0;
 }
